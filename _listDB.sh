bf1bc634ac63074eb8a98cd6f3273c40b42476ff
Zeinaaymann <84864225+Zeinaaymann@users.noreply.github.com>
2022-01-14 00:32:26 +0200

    Delete _listDB.sh

--- _listDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# check if DB_shell has databases or not 
-has_dbs=$(ls /home/$LOGNAME/DB_shell)
-if [ "$has_dbs" ]; then
-    ls `/home/$LOGNAME/DB_shell` 
-else
-    echo "There is no databases to show!"
-fi
